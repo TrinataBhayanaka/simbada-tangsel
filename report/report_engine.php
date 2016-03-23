b9a3543a9aca0fe28970ba1844fcd00eef389b15
ilmawan <ilmawan.muharam@gmail.com>
2016-03-24 04:48:11 +0700

    update lap mutasi barang

--- report/report_engine.php
+++ report/report_engine.php
@@ -29234,7 +29234,7 @@ $body="
 
                                              //udah dites
 					// $barangTotal = $barangTotal + $row->Kuantitas;
-					$perolehanTotal = $perolehanTotal + $row->NilaiPerolehan_Awal;
+					// $perolehanTotal = $perolehanTotal + $row->NilaiPerolehan_Awal;
 					// $perolehan = number_format($row->NilaiPerolehan);
 					
 					// $konstruksi_tanah= $this->get_konstruksi($row->Konstruksi);
