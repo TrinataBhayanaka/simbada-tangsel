cb4b39499f4c0ea260c6b20192e0abb3f4318298
ilmawan <ilmawan.muharam@gmail.com>
2017-03-08 11:49:08 +0700

    update penghapusan

--- module/penghapusan/validasi_proses_pmd.php
+++ module/penghapusan/validasi_proses_pmd.php
@@ -4,7 +4,7 @@ include "../../config/config.php";
 
 $PENGHAPUSAN = new RETRIEVE_PENGHAPUSAN;
 
-$menu_id = 40;
+$menu_id = 10;
 $SessionUser = $SESSION->get_session_user();
 ($SessionUser['ses_uid']!='') ? $Session = $SessionUser : $Session = $SESSION->get_session(array('title'=>'GuestMenu', 'ses_name'=>'menu_without_login')); 
 $USERAUTH->FrontEnd_check_akses_menu($menu_id, $SessionUser);
